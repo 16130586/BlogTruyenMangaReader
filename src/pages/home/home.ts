0f941b4eac01e9aa34767f887b58a100499272ac
ngctht <nnt09021998@gmail.com>
2017-07-20 17:04:15 +0700

    content hompage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  imgs: Array<{ url: string }>;
   constructor(public navCtrl: NavController) {
+    this.imgs = [{ url: "http://1.bp.blogspot.com/-TBMbCD4ZGQc/V_-BlKIKcgI/AAAAAAASDJ4/z82IOuNtoS4/s182-c/the-scholars-reincarnation.png"}];
+//   { url: "http://1.bp.blogspot.com/-TBMbCD4ZGQc/V_-BlKIKcgI/AAAAAAASDJ4/z82IOuNtoS4/s182-c/the-scholars-reincarnation.png"},
+// { url: "http://1.bp.blogspot.com/-TBMbCD4ZGQc/V_-BlKIKcgI/AAAAAAASDJ4/z82IOuNtoS4/s182-c/the-scholars-reincarnation.png"},
+// { url: "http://1.bp.blogspot.com/-TBMbCD4ZGQc/V_-BlKIKcgI/AAAAAAASDJ4/z82IOuNtoS4/s182-c/the-scholars-reincarnation.png"},
+// { url: "http://1.bp.blogspot.com/-TBMbCD4ZGQc/V_-BlKIKcgI/AAAAAAASDJ4/z82IOuNtoS4/s182-c/the-scholars-reincarnation.png"},
+// { url: "http://1.bp.blogspot.com/-MXOlMJJ160c/V-kFBEs4w3I/AAAAAAAR6fw/7SAiBJrE3N4/s182-c/Soi.jpg"},
+// { url: "http://1.bp.blogspot.com/-MXOlMJJ160c/V-kFBEs4w3I/AAAAAAAR6fw/7SAiBJrE3N4/s182-c/Soi.jpg"},
+// { url: "http://1.bp.blogspot.com/-MXOlMJJ160c/V-kFBEs4w3I/AAAAAAAR6fw/7SAiBJrE3N4/s182-c/Soi.jpg"}];
+   
 
   }
-
 }
